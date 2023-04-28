e45bbca13bf4ff805468d8b01470251153a8e679
rokibulslam <rokibulslam6@gmail.com>
2023-04-28 11:40:47 +0600

    xml to json

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, {useState} from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const App = () => {
+  const [jsonData, setJsonData] = useState(null);
+  console.log(jsonData.purchase_order['@attributes']);
+ const [xmlData, setXmlData] = useState("");
+  const handleFileSelect = async (event) => {
+    const file = event.target.files[0];
+    const fileReader = new FileReader();
+    fileReader.readAsText(file);
+    fileReader.onload = async () => {
+         const data = fileReader.result;
+         setXmlData(data);
+         const parser = new DOMParser();
+         const xmlDoc = parser.parseFromString(data, "text/xml");
+         const json = xmlToJson(xmlDoc);
+        setJsonData(json);
+    };
+  };
+    const xmlToJson = (xml) => {
+      const obj = {};
+      if (xml.nodeType === 1) {
+        if (xml.attributes.length > 0) {
+          obj["@attributes"] = {};
+          for (let i = 0; i < xml.attributes.length; i++) {
+            const attribute = xml.attributes.item(i);
+            obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+          }
+        }
+      } else if (xml.nodeType === 3) {
+        obj["#text"] = xml.nodeValue;
+      }
+      if (xml.hasChildNodes()) {
+        for (let i = 0; i < xml.childNodes.length; i++) {
+          const item = xml.childNodes.item(i);
+          const nodeName = item.nodeName;
+          if (typeof obj[nodeName] === "undefined") {
+            obj[nodeName] = xmlToJson(item);
+          } else {
+            if (typeof obj[nodeName].push === "undefined") {
+              const old = obj[nodeName];
+              obj[nodeName] = [];
+              obj[nodeName].push(old);
+            }
+            obj[nodeName].push(xmlToJson(item));
+          }
+        }
+      }
+      return obj;
+    };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Input</h1>
+      <input type="file" accept=".xml" onChange={(e)=>handleFileSelect(e)} />
+      {jsonData ? (
+        <div>
+          <h1></h1>
+          
+          <ul>
+            
+          </ul>
+        </div>
+      ) : (
+        <p>Please select an XML file.</p>
+      )}
+    </div>
+  );
 }
 
 export default App
+
+
+
+   
